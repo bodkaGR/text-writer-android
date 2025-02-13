da54c734882da47c0c69ae54d7dbe3ed6ee23e0e
Bohdan <grontkovskijboda@gmail.com>
2025-02-13 01:21:17 +0100

    text size selection implemented

--- app/src/main/java/com/bodkasoft/textwriter/MainActivity.java
+++ app/src/main/java/com/bodkasoft/textwriter/MainActivity.java
@@ -1,22 +1,31 @@
 package com.bodkasoft.textwriter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup textSizeRadioGroup;
     private Button buttonOkView, buttonCancelView;
     private EditText textInputView;
     private TextView resultTextView;
+    private String resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +34,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Retrieving all View components from Activity
+        initializeViews();
+
+        setupButtonListeners();
+
+        setupRadioGroupListener();
+    }
+
+    private void initializeViews() {
         buttonOkView = findViewById(R.id.buttonOkView);
         buttonCancelView = findViewById(R.id.buttonCancelView);
         textInputView = findViewById(R.id.textInputView);
         resultTextView = findViewById(R.id.resultTextView);
+        textSizeRadioGroup = findViewById(R.id.textSizeRadioGroup);
+    }
 
-        // Add action listener on "OK" button
-        buttonOkView.setOnClickListener(view -> {
-            String resultText = textInputView.getText().toString();
-            resultTextView.setText(resultText);
-        });
+    private void setupButtonListeners() {
+        Map<Button, View.OnClickListener> buttonConfigurator = Map.of(
+                // Add action listener for "OK" button
+                buttonOkView, (view) -> {
+                    resultText = textInputView.getText().toString();
+                    resultTextView.setText(resultText);
+                },
+                // Add action listener for "Cancel" button
+                buttonCancelView, (view) -> {
+                    resultTextView.setText("");
+                    textInputView.setText("");
+                }
+        );
 
-        // Add action listener on "Cancel" button
-        buttonCancelView.setOnClickListener(view -> {
-            resultTextView.setText("");
-            textInputView.setText("");
-        });
+        buttonConfigurator.forEach(Button::setOnClickListener);
+    }
+
+    private void setupRadioGroupListener() {
+        textSizeRadioGroup.setOnCheckedChangeListener(((group, checkedId) -> {
+            RadioButton textSizeRadioButton = findViewById(checkedId);
+            int textSize = extractTextSize(textSizeRadioButton.getText().toString());
+            resultTextView.setTextSize(textSize);
+            Log.d("RADIO", "Selected text size: " + textSizeRadioButton.getText().toString());
+            Snackbar.make(findViewById(android.R.id.content), "Selected text size: " + textSizeRadioButton.getText().toString(), Snackbar.LENGTH_SHORT).show();
+        }));
+    }
+
+    private static int extractTextSize(String str) {
+        Pattern pattern = Pattern.compile("^\\d+");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find() ? Integer.parseInt(matcher.group()) : 0;
     }
 }
